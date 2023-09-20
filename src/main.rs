74f867fa4d30ecb856095733aba5187b8a083f4c
gemhung <gembright.stone.hung@gmail.com>
2023-09-20 14:26:59 +0900

    pre-push 2

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,9 @@ use oneblog::error;
 #[tokio::main]
 async fn main() -> Result<(), crate::error::OneBlogError> {
     a
+        b
     oneblog::launch::launch_until_stopped().await?;
+
 
     Ok(())
 }
