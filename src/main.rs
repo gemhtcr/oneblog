53539b19582bf1c72cbb2e56571f09e8d93bb34c
gemhung <gembright.stone.hung@gmail.com>
2023-09-21 09:49:42 +0900

    fix compiler error

--- src/main.rs
+++ src/main.rs
@@ -2,9 +2,6 @@ use oneblog::error;
 
 #[tokio::main]
 async fn main() -> Result<(), crate::error::OneBlogError> {
-    a
-        b
-        c
     oneblog::launch::launch_until_stopped().await?;
 
 
